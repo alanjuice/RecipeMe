f1f49e7a7b6cf81837c039b214f0998929a7349b
Alan <93069058+alanjuice@users.noreply.github.com>
2024-08-25 13:11:32 +0530

    fix create recipe

--- recipeme/recipe/views.py
+++ recipeme/recipe/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponseRedirect
-from .forms import CreateRecipeForm
+
+from chef.models import Chef
 from .models import Recipe
+
+from .forms import CreateRecipeForm
+
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -10,14 +13,17 @@ def create(request):
     if request.method=="POST":
         formdata=CreateRecipeForm(request.POST)
         
-        if formdata.is_valid() and request.user.is_authenticated():
+        if formdata.is_valid() and request.user.is_authenticated:
             #getting fields
             name=formdata.cleaned_data["name"]
             difficulty=formdata.cleaned_data["difficulty"]
 
-            recipe = Recipe(recipe_name=name,difficulty=difficulty)
+            chef=Chef.objects.get(username=request.user.username)
+
+            recipe = Recipe(recipe_name=name,difficulty=difficulty,created_by=chef)
             recipe.save()
-            return redirect("/recipe")
+
+            return redirect("chef:account")
     
     form=CreateRecipeForm()
     context={
