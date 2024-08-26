41704f5796dbffacfd73190b42bcb78a822e3f1f
Alan <93069058+alanjuice@users.noreply.github.com>
2024-08-26 20:11:43 +0530

    add basic pagination

--- recipeme/home/urls.py
+++ recipeme/home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name="home"
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("recipes/<int:page_no>",views.recipe_page,name="recipes_view")
 ]
--- recipeme/home/views.py
+++ recipeme/home/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from recipe.models import Recipe
 
 # Create your views here.
 def index(request):
     return render(request,"home/index.html")
+
+
+def recipe_page(request,page_no):
+    recipes=Recipe.objects.all().order_by("recipe_name")
+    paginator=Paginator(recipes,10)
+    
+    page_recipes=paginator.get_page(page_no)
+    context={
+        "recipes":page_recipes
+    }
+    print(page_recipes)
+    return render(request,"home/recipes.html",context)
